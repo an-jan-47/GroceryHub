7b342063ded8d229dffdad1835b02498a0b5a651
an-jan-47 <anjankumar102938@gmail.com>
2025-08-14 01:34:29 +0530

    vercel054

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
